5c6e8a556f38e813f57be66dcb6b72cbf459898a
Henrique Hiram Libutti Nunez <henrique.nunez@yahoo.com>
2021-03-27 11:31:54 -0300

    Fail: stereo calibration

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
-#include <opencv2/opencv.hpp>
-#include <fcntl.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <stdint.h>
+
 #include <linux/videodev2.h>
 
-#include <libfreenect_sync.h>
+#include <libfreenect.h>
+#include "../libs/libfreenect_sync.h"
+#include <libfreenect_registration.h>
+
+#include <opencv2/opencv.hpp>
 
 #ifndef VID_WIDTH
 #define VID_WIDTH 640
@@ -15,46 +22,88 @@
 #endif
 
 #define VIDEO_IN  "/dev/video0"
-#define VIDEO_OUT "/dev/video6"
+#define VIDEO_OUT "/dev/video3" //TODO: make generic
 
 //output device.
 int output;
+bool should_run = true;
 
 size_t framesize = VID_WIDTH * VID_HEIGHT * 3;
 
+freenect_context* fn_ctx;
+freenect_device* fn_dev;
+
+// Buffers
+uint16_t *depth_buf;
+uint8_t *vid_buf;
+
+void depth_cb(freenect_device* dev, void* data, uint32_t timestamp)
+{
+    // Here we must change the buffer a little bit. Cause depth and video have
+    // different sizes.
+    memcpy(depth_buf, data, sizeof(uint16_t) * VID_HEIGHT * VID_WIDTH);
+}
+
+void vid_cb(freenect_device* dev, void* data, uint32_t timestamp)
+{
+    memcpy(vid_buf, data, 3 * sizeof(uint8_t) * VID_HEIGHT * VID_WIDTH);
+}
+
 void quit_func()
 {
-    printf("deu ruim\n");
+    printf("Something wrong happened!\n");
     exit(1);
 }
 
-int my_looop()
+void signal_handler(int signal)
 {
-    short *depth = 0;
-    char *rgb = 0;
-    uint32_t ts;
+    if (signal == SIGINT
+    ||	signal == SIGTERM)
+	should_run = false;
+}
 
-    while(1)
+int get_frames()
+{
+    uint16_t *depth = 0;
+    uint8_t *rgb = 0;			    //3 since it's RGB
+    uint8_t *registered_rgb = (uint8_t*) malloc(3 * VID_HEIGHT * VID_WIDTH);
+    uint32_t ts;
+    printf("Working...\n");
+    freenect_device* kinect;
+
+    //Setting LED color. Also will start the loop.
+    freenect_sync_set_led(LED_BLINK_RED_YELLOW , 0);
+    
+    //Getting reference after init.
+    kinect = freenect_sync_get_device_ref(0);
+    printf("Kinect device pointer: %p\n", kinect);
+
+    
+    while(should_run)
     {
-        printf("afes\n");
         if (freenect_sync_get_depth((void**)&depth, &ts, 0, FREENECT_DEPTH_11BIT) < 0)
             quit_func();
         
         if (freenect_sync_get_video((void**)&rgb, &ts, 0, FREENECT_VIDEO_RGB) < 0)
             quit_func();
 
-        printf("[%d][%d][%d][%d][%d][%d][%d][%d]\n[%d][%d][%d][%d][%d][%d][%d][%d]\n", depth[100], depth[200], depth[300], depth[400], depth[500], depth[600], depth[700], depth[800], depth[900], depth[1000], depth[1100], depth[1200], depth[1300], depth[1400], depth[1500], depth[1600]);
+	printf("D: %p RGB: %p RRGB: %p\n", depth, rgb, registered_rgb);
+	freenect_map_rgb_to_depth(kinect, depth, rgb, registered_rgb);
 
+	//memcpy(registered_rgb, rgb, sizeof(uint8_t) * 3 * VID_HEIGHT * VID_WIDTH);
+	// Stereo correction.
+	printf("mapped\n");
+
+	//Applies the threshold onto the frame.
         for (int i = 0; i < VID_HEIGHT; ++i)
         {
             for (int j = 0; j < VID_WIDTH; ++j)
-            {
-                
+            {        
                 if(depth[VID_WIDTH * i + j] > 700)
                 {
-                    rgb[(VID_WIDTH * i + j) * 3] = 0;
-                    rgb[(VID_WIDTH * i + j) * 3 + 1] = 0xFF;
-                    rgb[(VID_WIDTH * i + j) * 3 + 2] = 0;
+                    registered_rgb[(VID_WIDTH * i + j) * 3] = 0;
+                    registered_rgb[(VID_WIDTH * i + j) * 3 + 1] = 0xFF;
+                    registered_rgb[(VID_WIDTH * i + j) * 3 + 2] = 0;
                 }
             }
         }
@@ -62,7 +111,7 @@ int my_looop()
         //generate opencv mat here.
         // grab frame
      
-        cv::Mat frame(VID_HEIGHT, VID_WIDTH, CV_8UC3, rgb);
+        cv::Mat frame(VID_HEIGHT, VID_WIDTH, CV_8UC3, registered_rgb);
         /*
         if (not cam.grab()) {
             std::cerr << "ERROR: could not read from camera!\n";
@@ -93,44 +142,34 @@ int my_looop()
             //break;
     }
 
+    free(registered_rgb);
+
     return 0;
 }
 
-int
-main(int argc, char* argv[]) {
-
-    // open and configure input camera (/dev/video0)
-    /*cv::VideoCapture cam(VIDEO_IN);
-    if (not cam.isOpened()) {
-        std::cerr << "ERROR: could not open camera!\n";
-        return -1;
-    }
-    cam.set(cv::CAP_PROP_FRAME_WIDTH, VID_WIDTH);
-    cam.set(cv::CAP_PROP_FRAME_HEIGHT, VID_HEIGHT);
-    */
-
+int main(int argc, char* argv[])
+{
+    /***OUTPUT STREAM CONFIG********/
     // open output device
     output = open(VIDEO_OUT, O_RDWR);
-    if(output < 0) {
+    if(output < 0)
+    {
         std::cerr << "ERROR: could not open output device!\n" << strerror(errno);
         return -2;
     }
-
+ 
     // configure params for output device
     struct v4l2_format vid_format;
     memset(&vid_format, 0, sizeof(vid_format));
     vid_format.type = V4L2_BUF_TYPE_VIDEO_OUTPUT;
 
-    if (ioctl(output, VIDIOC_G_FMT, &vid_format) < 0) {
+    if (ioctl(output, VIDIOC_G_FMT, &vid_format) < 0)
+    {
         std::cerr << "ERROR: unable to get video format!\n" << strerror(errno);
         return -1;
     }
 
-    //vid_format.fmt.pix.width = cam.get(cv::CAP_PROP_FRAME_WIDTH);
-    //vid_format.fmt.pix.height = cam.get(cv::CAP_PROP_FRAME_HEIGHT);
-
-    // NOTE: change this according to below filters...
-    // Chose one from the supported formats on Chrome:
+    // NOTE: Available pixel formats.
     // - V4L2_PIX_FMT_YUV420,
     // - V4L2_PIX_FMT_Y16,
     // - V4L2_PIX_FMT_Z16,
@@ -148,9 +187,16 @@ main(int argc, char* argv[]) {
         std::cerr << "ERROR: unable to set video format!\n" << strerror(errno);
         return -1;
     }
+    /***END OUTPUT STREAM CONFIG***/
+
+    /***KINECT CONFIGURATION*******/
+    //freenect_context
+
+    /***END KINECT CONFIGURATION***/
 
     // loop over these actions:
-    my_looop();
-
+    get_frames();
     std::cout << "\n\nFinish, bye!\n";
+    freenect_sync_stop();
+
 }
